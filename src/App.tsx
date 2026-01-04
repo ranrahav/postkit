0a1f890bb63b49d3da986019bb911aee6b5e0fe2
Ran Rahav <ranrahav@gmail.com>
2026-01-04 18:50:53 +0200

    Update App.tsx to use DashboardSimplified component
    - Ensure production routes to new simplified dashboard
    - All new UI improvements (left panel, iOS selection, integrated send button) will be live

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,7 @@ import Landing from "./pages/Landing";
 import Auth from "./pages/Auth";
 import AuthCallback from "./pages/AuthCallback";
 import ResetPassword from "./pages/ResetPassword";
-import Dashboard from "./pages/Dashboard";
-import Create from "./pages/Create";
-import EditCarousel from "./pages/EditCarousel";
-import MyCarousels from "./pages/MyCarousels";
+import Dashboard from "./pages/DashboardSimplified";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -27,10 +24,6 @@ const App = () => (
           <Route path="/auth/callback" element={<AuthCallback />} />
           <Route path="/reset-password" element={<ResetPassword />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/create" element={<Create />} />
-          <Route path="/edit/:id" element={<EditCarousel />} />
-          <Route path="/my-carousels" element={<MyCarousels />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
